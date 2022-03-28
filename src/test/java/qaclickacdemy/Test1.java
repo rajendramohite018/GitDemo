f87535535ca8fa238c96460a55a5315a602af94e
Rajendra <rajendra.mohite018@gmail.com>
2022-03-28 11:46:45 +0530

    first commit

---
+++ src/test/java/qaclickacdemy/Test1.java
@@ -0,0 +1,11 @@
+package qaclickacdemy;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+	
+	@Test
+	public void a() {
+		System.out.println("a is method name");
+	}
+}
---
+++ src/test/java/qaclickacdemy/Test2.java
@@ -0,0 +1,16 @@
+package qaclickacdemy;
+
+import org.testng.annotations.Test;
+
+public class Test2 {
+
+	@Test
+	public void b() {
+		System.out.println("b is method name");
+	}
+	
+	@Test
+	public void d() {
+		System.out.println("d is method name");
+	}
+}
