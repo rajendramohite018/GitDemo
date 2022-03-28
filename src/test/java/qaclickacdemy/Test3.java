8f8330c50ec5b0d7883d44bbbc9cef9a44ddc9b7
Rajendra <rajendra.mohite018@gmail.com>
2022-03-28 13:18:37 +0530

    architect devloep one test case

---
+++ src/test/java/qaclickacdemy/Test3.java
@@ -0,0 +1,12 @@
+package qaclickacdemy;
+
+import org.testng.annotations.Test;
+
+public class Test3 {
+
+	
+	@Test
+	public void develop() {
+		System.out.println("devloped branch and checkig for merge confiltsc");
+	}
+}
